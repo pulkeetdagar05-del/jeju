765f7416952976b87065f61f3f4459c4486def23
DarkThor76 <39576038+DarkThor76@users.noreply.github.com>
2024-10-06 23:19:12 -0400

    Update TimerBar.js
    "<0%" makes no sense. Should be offset by 0.01 (1%). Honestly, probably do not need the call to getChanceFinal() in the output either but I'll leave that up to someone more authoritative.

--- src/Disp/InfoBars/TimerBar.js
+++ src/Disp/InfoBars/TimerBar.js
@@ -133,7 +133,7 @@ export function UpdateTimerBar() {
       updateChanceTotal(chanceToSpawn);
       l('CMTimerBarGCTime').textContent = `${Math.ceil(
         (Game.shimmerTypes.golden.maxTime - Game.shimmerTypes.golden.time) / Game.fps,
-      )} ${getChanceFinal() < 0.01 ? '<' : ''}${getChanceFinal().toLocaleString('en', {
+      )} ${getChanceFinal() < 0.01 ? '<' : ''}${(getChanceFinal() + 0.01).toLocaleString('en', { // "<0%" makes no sense. Should be offset by 0.01 (1%).
         style: 'percent',
       })}`;
       numberOfTimers += 1;
