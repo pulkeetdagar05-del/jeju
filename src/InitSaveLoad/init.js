989e984eb5eea2850b890df43ddd38ff552c887f
Kevin Radloff <radsaq@gmail.com>
2025-02-17 08:01:27 -0500

    Remove Cookie Clicker version check and the need for user confirmation.

--- src/InitSaveLoad/init.js
+++ src/InitSaveLoad/init.js
@@ -1,5 +1,4 @@
 import { initFunctions } from '@cookiemonsterteam/cookiemonsterframework';
-import { VersionMajor, VersionMinor } from '../Data/Moddata.js';
 import CMDrawHook from '../Disp/DrawHook.js';
 import CMClickHook from '../Main/ClickHook.js';
 import InitializeCookieMonster from '../Main/Initialization.js';
@@ -13,28 +12,18 @@ import load from './load.js';
  * It starts the further initialization of CookieMonster and registers hooks
  */
 export default function init() {
-  let proceed = true;
-
   // Load Cookie Monster Mod Framework and register mod
   initFunctions.initModFramework();
   window.cookieMonsterFrameworkData.isInitializing = true;
   initFunctions.registerMod('cookieMonsterMod');
 
-  if (Game.version !== Number(VersionMajor)) {
-    // eslint-disable-next-line no-restricted-globals, no-alert
-    proceed = confirm(
-      `Cookie Monster version ${VersionMajor}.${VersionMinor} is meant for Game version ${VersionMajor}. Loading a different version may cause errors. Do you still want to load Cookie Monster?`,
-    );
-  }
-  if (proceed) {
-    InitializeCookieMonster();
-    Game.registerHook('click', CMClickHook);
-    Game.registerHook('draw', CMDrawHook);
-    Game.registerHook('logic', CMLoopHook);
+  InitializeCookieMonster();
+  Game.registerHook('click', CMClickHook);
+  Game.registerHook('draw', CMDrawHook);
+  Game.registerHook('logic', CMLoopHook);
 
-    // Load default settings if no previous saveData is found
-    if (typeof Game.modSaveData.cookieMonsterMod === 'undefined') {
-      load('{}');
-    }
+  // Load default settings if no previous saveData is found
+  if (typeof Game.modSaveData.cookieMonsterMod === 'undefined') {
+    load('{}');
   }
 }
