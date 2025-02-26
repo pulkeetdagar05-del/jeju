4e6c828f8b816641f6fce8464fc3a91be9c8146e
Kevin Radloff <radsaq@gmail.com>
2025-02-26 07:48:56 -0500

    Minor refactor to remove unused probability functions and combine functions that were only used in one spot.

--- src/Main/CheckStates/Probability.js
+++ src/Main/CheckStates/Probability.js
@@ -12,39 +12,22 @@ export function resetChanceTotalDeer() {
   chanceTotalDeer = 1.0;
 }
 
-export function getChanceTotal() {
-  return chanceTotal;
-}
-
-export function getChanceTotalDeer() {
-  return chanceTotalDeer;
-}
 /**
  * Update the probability that a cookie has not spawned
  * @param {number} chanceToSpawn The probablity that a GC appears
+ * @returns the cumulative probability that a GC has appeared from beginning to now
  */
 export function updateChanceTotal(chanceToSpawn) {
   chanceTotal *= 1 - chanceToSpawn;
+  return 1 - chanceTotal;
 }
 
 /**
  * Update the probability that a reindeer has not spawned
  * @param {number} chanceToSpawn The probablity that a reindeer appears
+ * @returns the cumulative probability that a reindeer has appeared from beginning to now
  */
 export function updateChanceTotalDeer(chanceToSpawn) {
   chanceTotalDeer *= 1 - chanceToSpawn;
-}
-/**
- *
- * @returns the cummulative probability that a GC has appeared from beginning to now
- */
-export function getChanceFinal() {
-  return 1 - chanceTotal;
-}
-/**
- *
- * @returns the cummulative probability that a reindeer has appeared from beginning to now
- */
-export function getChanceFinalDeer() {
   return 1 - chanceTotalDeer;
 }
